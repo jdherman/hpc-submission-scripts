243ef6adb5b6f14ba0e37c110f7940797b6dc522
YU LI <likymice@gmail.com>
2015-01-28 10:30:44 -0500

    - add some comments to justify the commands

--- pbs_batch_job_submission/wrapper.sh
+++ pbs_batch_job_submission/wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# this the shell script for wrapping your program
 
+# 'rank' changes with the ongoing no. of the process 
 rank=`expr ${OMPI_COMM_WORLD_RANK} + $1`
 
+# define your batch job here
 touch output_${rank}.out 
 echo "hello, this is no ${rank} file" > output_${rank}.out
